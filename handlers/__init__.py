6c8de669704c0d92fcf854c728fc2390b8f157ef
vinixspb <60535818+vinixspb@users.noreply.github.com>
2026-02-12 23:13:23 +0200

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,6 @@
+from .admin import start
+from .chat import handle_text
+from .media import handle_photo
+from .audio import handle_voice
+from .callback_router import handle_callback
+from .chat import handle_document
